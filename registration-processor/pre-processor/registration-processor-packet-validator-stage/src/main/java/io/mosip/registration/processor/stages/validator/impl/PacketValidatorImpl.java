1e4e9e9ca9892d637ea09e1cebb8ff210a348ca6
SowmyaGoudar <40026315+SowmyaGoudar@users.noreply.github.com>
2024-10-30 12:10:39 +0530

    Tf nira version (#17)
    * fixed nin lower case to fetch by idrepo
    
    * fixed nin lower case to fetch by idrepo
    
    * fixed nin lower case to fetch by idrepo
    
    * fixed
    
    * uin generator stage fix
    
    * code changes for renewal notification
    
    * fixed for update and renewa;
    
    * updated version
    
    * updated version
    
    * updated version
    
    * updated pom version
    
    * corrected version
    
    * added changes
    
    ---------
    
    Co-authored-by: Sowmya U B <sowmya.ub@technoforte.co.in>

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/validator/impl/PacketValidatorImpl.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/validator/impl/PacketValidatorImpl.java
@@ -111,7 +111,8 @@ public class PacketValidatorImpl implements PacketValidator {
 			
 
 			if (process.equalsIgnoreCase(RegistrationType.UPDATE.toString())
-					|| process.equalsIgnoreCase(RegistrationType.RES_UPDATE.toString())) {
+					|| process.equalsIgnoreCase(RegistrationType.RES_UPDATE.toString())
+					|| process.equalsIgnoreCase(RegistrationType.RENEWAL.toString())) {
 				uin = utility.getUINByHandle(id, process, ProviderStageName.PACKET_VALIDATOR);
 				if (uin == null) {
 					regProcLogger.error(LoggerFileConstant.SESSIONID.toString(),
@@ -147,7 +148,8 @@ public class PacketValidatorImpl implements PacketValidator {
 
 			// check if uin is in idrepisitory
 			if (RegistrationType.UPDATE.name().equalsIgnoreCase(process)
-					|| RegistrationType.RES_UPDATE.name().equalsIgnoreCase(process)) {
+					|| RegistrationType.RES_UPDATE.name().equalsIgnoreCase(process)
+					|| process.equalsIgnoreCase(RegistrationType.RENEWAL.toString())) {
 
 				if (!utility.uinPresentInIdRepo(String.valueOf(uin))) {
 					regProcLogger.error(LoggerFileConstant.SESSIONID.toString(),
