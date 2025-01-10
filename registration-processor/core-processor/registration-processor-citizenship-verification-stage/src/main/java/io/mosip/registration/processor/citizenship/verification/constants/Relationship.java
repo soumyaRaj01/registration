3f63ef23a0aa2f31d08d05d83824586b23c4c3d0
Brian Zimbe <93902372+BrianZimbe@users.noreply.github.com>
2025-01-10 16:31:35 +0300

    updated logic for Citizenship Verfication Stage.

--- registration-processor/core-processor/registration-processor-citizenship-verification-stage/src/main/java/io/mosip/registration/processor/citizenship/verification/constants/Relationship.java
+++ registration-processor/core-processor/registration-processor-citizenship-verification-stage/src/main/java/io/mosip/registration/processor/citizenship/verification/constants/Relationship.java
@@ -3,11 +3,17 @@ package io.mosip.registration.processor.citizenship.verification.constants;
 public enum Relationship {
 	FATHER("Father"), //tested and complete
 	MOTHER("Mother"), //tested and complete
+	
+	GRAND_FATHER_ON_FATHERS_SIDE("Grandfather on Father's side"),//tested and complete
+	GRAND_FATHER_ON_MOTHERS_SIDE ("Grandfather on Mother's side"),
+	
     GRAND_MOTHER_ON_FATHERS_SIDE("Grandmother on Father's side"),
-    GRAND_FATHER_ON_FATHERS_SIDE("Grandfather on Father's side"), //tested and complete
-    MATERNAL_UCLE_OR_AUNT("Maternal Uncle or Aunt"),
-    PATERNAL_UCLE_OR_AUNT("Paternal Uncle or Aunt"),
-	BROTHER_OR_SISTER("Sister or Brother"); //tested and complete
+    GRAND_MOTHER_ON_MOTHERS_SIDE("Grandmother on Mother's side"),
+    
+    MATERNAL_UNCLE_OR_AUNT("Maternal Uncle or Aunt"),
+    PATERNAL_UNCLE_OR_AUNT("Paternal Uncle or Aunt"),
+	BROTHER_OR_SISTER("Sister or Brother"),//tested and complete
+	FIRST_COUSIN ("First Cousin");
 
     private final String relation;
 
