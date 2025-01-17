1439218c1072ca2d4f29b7b6b0ae26f6c2c921bc
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-01-17 09:18:43 +0530

    Fixed test cases

--- registration-processor/core-processor/registration-processor-mvs-stage/src/test/java/io/mosip/registration/processor/verification/service/VerificationServiceTest.java
+++ registration-processor/core-processor/registration-processor-mvs-stage/src/test/java/io/mosip/registration/processor/verification/service/VerificationServiceTest.java
@@ -149,7 +149,7 @@ public class VerificationServiceTest {
 
 
 	private InternalRegistrationStatusDto registrationStatusDto;
-	private SyncRegistrationEntity regEntity;
+	private List<SyncRegistrationEntity> regEntities;
 	private VerificationPKEntity PKId;
 	private ManualVerificationDTO manualVerificationDTO;
 	private MatchDetail matchDetail=new MatchDetail();
@@ -223,8 +223,10 @@ public class VerificationServiceTest {
 
 		manualVerificationDTO = new ManualVerificationDTO();
 		registrationStatusDto = new InternalRegistrationStatusDto();
-		regEntity = new SyncRegistrationEntity();
+		regEntities = new ArrayList<>();
+		SyncRegistrationEntity regEntity = new SyncRegistrationEntity();
 		regEntity.setReferenceId("1234");
+		regEntities.add(regEntity);
 		dto = new UserDto();
 
 		PKId = new VerificationPKEntity();
@@ -333,8 +335,8 @@ public class VerificationServiceTest {
 		Mockito.when(env.getProperty(ApiName.DATASHARECREATEURL.name())).thenReturn("/v1/datashare/create");
 		Mockito.when(registrationStatusService.getRegistrationStatus(anyString(), any(), any(), any()))
 				.thenReturn(registrationStatusDto);
-		Mockito.when(syncRegistrationService.findByWorkflowInstanceId(anyString()))
-		.thenReturn(regEntity);
+		Mockito.when(syncRegistrationService.findByRegistrationId(anyString()))
+		.thenReturn(regEntities);
 
 		Mockito.when(packetManagerService.getFields(anyString(), any(), anyString(), any())).thenReturn(identity);
 		Mockito.when(packetManagerService.getBiometrics(anyString(), anyString(), any(), anyString(), any()))
